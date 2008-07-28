06f6710547d9763624c84ffb36aa14f20b4d8ab0
Markus Heiden <markus@markusheiden.de>
2008-07-28 20:37:08 +0000

    more performance test options

--- src/de/markusheiden/c64dt/simulator/PerformanceTest.java
+++ src/de/markusheiden/c64dt/simulator/PerformanceTest.java
@@ -1,15 +1,23 @@
 package de.markusheiden.c64dt.simulator;
 
+import java.util.concurrent.CyclicBarrier;
+
 /**
  * Test threading performance.
  */
 public class PerformanceTest {
-  private int i;
+  public static long DURATION = 60 * 1000;
+
+  private long i1;
+  private long i2;
   private volatile boolean run;
+  private volatile long start;
+  private volatile long end;
 
   public static void main(String[] args) throws Exception {
-    new PerformanceTest().start();
-    new PerformanceTest().startThread();
+//    new PerformanceTest().start();
+//    new PerformanceTest().startThread();
+    new PerformanceTest().startBarrier();
   }
 
   public void start() throws Exception {
@@ -20,31 +28,32 @@ public class PerformanceTest {
           synchronized (lock) {
             lock.wait();
           }
+          start = System.currentTimeMillis();
           while(run) {
-            inc();
+            for (int j = 0; j < 100; j++) {}
+            inc1();
           }
         } catch (InterruptedException e) {
         }
+        end = System.currentTimeMillis();
       }
     });
 
     thread.start();
-    Thread.sleep(1000);
+    Thread.sleep(2000);
 
     run = true;
     synchronized (lock) {
       lock.notify();
     }
 
-    Thread.sleep(1000);
+    Thread.sleep(DURATION);
     run = false;
     thread.join();
 
-    System.out.println("i = " + i);
-  }
-
-  protected void inc() {
-    i++;
+    long duration = end - start;
+    long incs = i1 * 1000 / duration;
+    System.out.println("inc/s = " + incs + " (" + duration + " ms)");
   }
 
   public void startThread() throws Exception {
@@ -53,14 +62,17 @@ public class PerformanceTest {
       public void run() {
         try {
           synchronized (lock) {
+            lock.wait();
+            start = System.currentTimeMillis();
             while(true) {
-              lock.wait();
-              inc();
+              inc1();
               lock.notify();
+              lock.wait();
             }
           }
         } catch (InterruptedException e) {
         }
+        end = System.currentTimeMillis();
       }
     });
     Thread thread2 = new Thread(new Runnable() {
@@ -70,7 +82,7 @@ public class PerformanceTest {
             synchronized (lock) {
               while(true) {
                 lock.wait();
-                inc();
+                inc1();
                 lock.notify();
               }
             }
@@ -81,19 +93,87 @@ public class PerformanceTest {
     });
 
     thread1.start();
+    Thread.sleep(2000);
     thread2.start();
-    Thread.sleep(1000);
+    Thread.sleep(2000);
 
     synchronized (lock) {
       lock.notify();
     }
 
-    Thread.sleep(10000);
+    Thread.sleep(DURATION);
     thread1.interrupt();
     thread2.interrupt();
     thread1.join();
     thread2.join();
 
-    System.out.println("i = " + i);
+    long duration = end - start;
+    long incs = i1 * 1000 / duration;
+    System.out.println("inc/s = " + incs + " (" + duration + " ms)");
+  }
+
+  public void startBarrier() throws Exception {
+    final CyclicBarrier barrier = new CyclicBarrier(2);
+    final Object lock1 = new Object();
+    final Object lock2 = new Object();
+    Thread thread1 = new Thread(new Runnable() {
+      public void run() {
+        try {
+          synchronized (lock1) {
+            lock1.wait();
+          }
+          start = System.currentTimeMillis();
+          while(true) {
+            inc1();
+            barrier.await();
+          }
+        } catch (Exception e) {
+        }
+        end = System.currentTimeMillis();
+      }
+    });
+    Thread thread2 = new Thread(new Runnable() {
+      public void run() {
+        try {
+          synchronized (lock2) {
+            lock2.wait();
+          }
+          while(true) {
+            inc2();
+            barrier.await();
+          }
+        } catch (Exception e) {
+        }
+      }
+    });
+
+    thread1.start();
+    thread2.start();
+    Thread.sleep(2000);
+
+    synchronized (lock1) {
+      synchronized (lock2) {
+        lock1.notifyAll();
+        lock2.notifyAll();
+      }
+    }
+
+    Thread.sleep(DURATION);
+    thread1.interrupt();
+    thread2.interrupt();
+    thread1.join();
+    thread2.join();
+
+    long duration = end - start;
+    long incs = (i1 + i2) * 1000 / duration;
+    System.out.println("inc/s = " + incs + " (" + duration + " ms)");
+  }
+
+  public void inc1() {
+    i1++;
+  }
+
+  public void inc2() {
+    i2++;
   }
 }
