753b917756ed7dba0b146f83b42f154eb28b6659
Markus Heiden <markus@markusheiden.de>
2010-01-04 21:09:37 +0000

    mh: moved screen component to c64dt

--- src/main/java/de/heiden/c64dt/browser/JC64TextField.java
+++ src/main/java/de/heiden/c64dt/browser/JC64TextField.java
@@ -1,6 +1,7 @@
 package de.heiden.c64dt.browser;
 
 import de.heiden.c64dt.util.ByteUtil;
+import de.heiden.c64dt.util.ResourceLoader;
 import org.springframework.util.Assert;
 import org.springframework.util.FileCopyUtils;
 
@@ -14,7 +15,7 @@ import java.io.InputStream;
  * Text display for C64 encoded text using a C64 font.
  */
 public class JC64TextField extends JComponent {
-  private static byte[] charRom;
+  private static int[] charRom;
   private int fontSize = 8;
   private byte[] text;
 
@@ -43,12 +44,10 @@ public class JC64TextField extends JComponent {
 
   private void init() {
     if (charRom == null) {
-      InputStream stream = getClass().getResourceAsStream("/roms/character/display.bin");
-      Assert.notNull(stream, "Precondition: display.bin exists in roms folder");
       try {
-        charRom = FileCopyUtils.copyToByteArray(stream);
+        charRom = ResourceLoader.load(0x1000, "/roms/character/display.bin");
       } catch (IOException e) {
-        Assert.isTrue(false, "Precondition: Char rom is readable");
+        Assert.isTrue(false, "Precondition: Char rom exists and is readable, but got: " + e.getMessage());
       }
     }
   }
@@ -82,7 +81,7 @@ public class JC64TextField extends JComponent {
     }
   }
 
-  protected final void drawLine(Graphics g, int x, int y, byte image) {
+  protected final void drawLine(Graphics g, int x, int y, int image) {
     for (int i = 0; i < 8; i++, x++) {
       if ((image & 0x80) != 0) {
         g.fillRect(x, y, 1, 1);
