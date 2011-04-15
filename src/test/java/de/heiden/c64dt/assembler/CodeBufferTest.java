9594d4ba4f039ea69fc70def30010f8afe4a1195
Markus Heiden <markus@markusheiden.de>
2011-04-15 09:56:34 +0200

    Added tests

--- src/test/java/de/heiden/c64dt/assembler/CodeBufferTest.java
+++ src/test/java/de/heiden/c64dt/assembler/CodeBufferTest.java
@@ -18,4 +18,22 @@ public class CodeBufferTest
     assertTrue(buffer.has(2));
     assertFalse(buffer.has(3));
   }
+
+  @Test
+  public void testReadByte() {
+    CodeBuffer buffer = new CodeBuffer(new byte[]{0x12});
+    assertEquals(0x12, buffer.readByte());
+    assertFalse(buffer.has(1));
+
+  }
+
+  @Test
+  public void testRead() {
+    CodeBuffer buffer = new CodeBuffer(new byte[]{0x12, 0x34, 0x56});
+    // Read byte
+    assertEquals(0x12, buffer.read(1));
+    // Read word
+    assertEquals(0x5634, buffer.read(2));
+    assertFalse(buffer.has(1));
+  }
 }
