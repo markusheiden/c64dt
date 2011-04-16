11b523bdc11418c365811d8c552af7ebf4305b25
Markus Heiden <markus@markusheiden.de>
2011-04-16 18:53:49 +0200

    Data command uses !FILL where possible

--- src/main/java/de/heiden/c64dt/assembler/command/DataCommand.java
+++ src/main/java/de/heiden/c64dt/assembler/command/DataCommand.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static de.heiden.c64dt.util.HexUtil.hex;
 import static de.heiden.c64dt.util.HexUtil.hexByte;
 
 /**
@@ -18,6 +19,11 @@ public class DataCommand extends AbstractCommand
 {
   private List<Integer> data;
 
+  /**
+   * Constructor.
+   *
+   * @param data byte this command represents
+   */
   public DataCommand(int data)
   {
     super(false);
@@ -26,16 +32,19 @@ public class DataCommand extends AbstractCommand
     this.data.add(data);
   }
 
+  @Override
   public final int getSize()
   {
     return data.size();
   }
 
+  @Override
   public final boolean isEnd()
   {
     return true;
   }
 
+  @Override
   public boolean combineWith(ICommand command)
   {
     if (!(command instanceof DataCommand))
@@ -47,28 +56,56 @@ public class DataCommand extends AbstractCommand
     return true;
   }
 
+  @Override
   public void toString(CommandBuffer buffer, Writer output) throws IOException
   {
     Assert.notNull(buffer, "Precondition: buffer != null");
     Assert.notNull(output, "Precondition: output != null");
 
-    output.append("!BYTE ");
-    output.append(hexByte(data.get(0)));
-    for (int i = 1; i < data.size(); i++)
-    {
+    if (data.size() > 8 && isSameByte()) {
+      // special case: the data consists of the same byte over and over again
+
+      output.append("!FILL ");
+      output.append(hex(data.size()));
       output.append(", ");
-      output.append(hexByte(data.get(i)));
+      output.append(hexByte(data.get(0)));
+
+    } else {
+      // default case: different data bytes
+
+      output.append("!BYTE ");
+      output.append(hexByte(data.get(0)));
+      for (int i = 1; i < data.size(); i++)
+      {
+        output.append(", ");
+        output.append(hexByte(data.get(i)));
+      }
+      output.append("; '");
+      byte[] string = new byte[data.size()];
+      for (int i = 0; i < data.size(); i++)
+      {
+        string[i] = data.get(i).byteValue();
+      }
+      output.append(C64Charset.LOWER.toString(string));
+      output.append("'");
     }
-    output.append("; '");
-    byte[] string = new byte[data.size()];
-    for (int i = 0; i < data.size(); i++)
-    {
-      string[i] = data.get(i).byteValue();
-    }
-    output.append(C64Charset.LOWER.toString(string));
-    output.append("'");
   }
 
+  /**
+   * Detect if the data consists of the same byte repeated multiple times.
+   */
+  private boolean isSameByte() {
+    Integer content = data.get(0);
+    for (Integer dataByte : data) {
+      if  (!content.equals(dataByte)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  @Override
   public List<Integer> toBytes()
   {
     return Collections.unmodifiableList(data);
