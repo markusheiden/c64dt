53d6533e7abde8a5d5eff28ed65edae79241fb4a
Markus Heiden <markus@markusheiden.de>
2011-04-16 11:50:31 +0200

    Reworked

--- src/main/java/de/heiden/c64dt/assembler/detector/LabelDetector.java
+++ src/main/java/de/heiden/c64dt/assembler/detector/LabelDetector.java
@@ -2,6 +2,7 @@ package de.heiden.c64dt.assembler.detector;
 
 import de.heiden.c64dt.assembler.CodeType;
 import de.heiden.c64dt.assembler.command.CommandBuffer;
+import de.heiden.c64dt.assembler.command.ICommand;
 
 /**
  * Detects code label as code and data label as data.
@@ -16,14 +17,14 @@ public class LabelDetector implements IDetector
     commands.restart();
     while (commands.hasNextCommand())
     {
-      commands.nextCommand();
+      ICommand command = commands.nextCommand();
       if (commands.hasCodeLabel())
       {
         // Mark all code label positions as a start of an opcode
         commands.setType(CodeType.OPCODE);
         change = true;
       }
-      else if (commands.getType().equals(CodeType.UNKNOWN) && commands.hasDataLabel())
+      else if (commands.hasDataLabel() && commands.getType().isUnknown())
       {
         // Mark all data label positions as data
         commands.setType(CodeType.DATA);
@@ -31,6 +32,6 @@ public class LabelDetector implements IDetector
       }
     }
 
-    return false;
+    return change;
   }
 }
