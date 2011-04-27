b1428f8ee86b9382cdd8325486a73138bb605c5d
Markus Heiden <markus@markusheiden.de>
2011-04-27 22:51:15 +0200

    Reworked reachability

--- src/main/java/de/heiden/c64dt/assembler/detector/Reachability.java
+++ src/main/java/de/heiden/c64dt/assembler/detector/Reachability.java
@@ -1,5 +1,8 @@
 package de.heiden.c64dt.assembler.detector;
 
+import de.heiden.c64dt.assembler.Opcode;
+import de.heiden.c64dt.assembler.OpcodeType;
+import de.heiden.c64dt.assembler.command.BitCommand;
 import de.heiden.c64dt.assembler.command.CommandBuffer;
 import de.heiden.c64dt.assembler.command.CommandIterator;
 import de.heiden.c64dt.assembler.command.DummyCommand;
@@ -20,39 +23,43 @@ public class Reachability implements IDetector
     for (CommandIterator iter = new CommandIterator(commands); iter.hasNextCommand();)
     {
       ICommand command = iter.nextCommand();
-      command.setReachable(command instanceof OpcodeCommand);
+      command.setReachable(command instanceof OpcodeCommand || command instanceof BitCommand);
     }
 
-    boolean change;
-    do
+    CommandIterator iter = new CommandIterator(commands).reverse();
+
+    // trace backward from unreachable command to the previous
+    ICommand lastCommand = new DummyCommand();
+    while (iter.hasPreviousCommand())
     {
-      CommandIterator iter = new CommandIterator(commands).reverse();
-      change = false;
-
-      // trace backward from unreachable command to the previous
-      ICommand lastCommand = new DummyCommand();
-      while (iter.hasPreviousCommand())
+      ICommand command = iter.previousCommand();
+      /*
+       * A code command is not reachable, if it leads to unreachable code.
+       * Exception is JSR, because its argument may follow directly after the instruction.
+       *
+       * TODO mh: check JMP/JSR/Bxx targets for reachability?
+       */
+      if (!lastCommand.isReachable() &&
+          command.isReachable() && !command.isEnd() && !isJsr(command) && !iter.getType().isCode())
       {
-        ICommand command = iter.previousCommand();
-        /*
-         * A code command is not reachable, if it leads to unreachable code.
-         *
-         * TODO mh: Exception: JSR which may be followed by argument data.
-         * TODO mh: check JMP/JSR/Bxx targets for reachability
-         */
-        if (!lastCommand.isReachable() &&
-          command.isReachable() && !command.isEnd() && !iter.getType().isCode())
-        {
-          command.setReachable(false);
-          // restart, if reference caused a wrong label in the already scanned code
-          change |= iter.removeReference();
-        }
-
-        lastCommand = command;
+        command.setReachable(false);
+        iter.removeReference();
       }
-    } while (change);
+
+      lastCommand = command;
+    }
 
     // code types have not been changed
     return false;
+  }
+
+  /**
+   * Check if command is a JSR.
+   *
+   * @param command Command
+   */
+  private boolean isJsr(ICommand command)
+  {
+    return command instanceof OpcodeCommand && ((OpcodeCommand) command).getOpcode().getType().equals(OpcodeType.JSR);
   }
 }
