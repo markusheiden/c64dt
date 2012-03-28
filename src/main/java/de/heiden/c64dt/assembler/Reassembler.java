20c418f3d69a99c01eb3fc57d4ab5ff9dd6e49f6
Markus Heiden <markus@markusheiden.de>
2012-03-28 23:46:41 +0200

    Fixed todo comments

--- src/main/java/de/heiden/c64dt/assembler/Reassembler.java
+++ src/main/java/de/heiden/c64dt/assembler/Reassembler.java
@@ -3,13 +3,7 @@ package de.heiden.c64dt.assembler;
 import de.heiden.c64dt.assembler.command.CommandBuffer;
 import de.heiden.c64dt.assembler.command.CommandIterator;
 import de.heiden.c64dt.assembler.command.ICommand;
-import de.heiden.c64dt.assembler.detector.BitDetector;
-import de.heiden.c64dt.assembler.detector.BrkDetector;
-import de.heiden.c64dt.assembler.detector.IDetector;
-import de.heiden.c64dt.assembler.detector.JsrDetector;
-import de.heiden.c64dt.assembler.detector.LabelDetector;
-import de.heiden.c64dt.assembler.detector.Reachability;
-import de.heiden.c64dt.assembler.detector.Tokenizer;
+import de.heiden.c64dt.assembler.detector.*;
 import de.heiden.c64dt.assembler.label.ExternalLabel;
 import de.heiden.c64dt.assembler.label.ILabel;
 import org.springframework.util.Assert;
@@ -28,17 +22,13 @@ import java.util.List;
 import java.util.TreeSet;
 
 import static de.heiden.c64dt.util.ByteUtil.toWord;
-import static de.heiden.c64dt.util.HexUtil.hex;
-import static de.heiden.c64dt.util.HexUtil.hexBytePlain;
-import static de.heiden.c64dt.util.HexUtil.hexWord;
-import static de.heiden.c64dt.util.HexUtil.hexWordPlain;
+import static de.heiden.c64dt.util.HexUtil.*;
 
 /**
  * Reassembler.
  */
 @XmlRootElement(name = "reassembler")
-public class Reassembler
-{
+public class Reassembler {
   /**
    * Code type detectors to use.
    */
@@ -62,8 +52,7 @@ public class Reassembler
   /**
    * Constructor.
    */
-  public Reassembler()
-  {
+  public Reassembler() {
     // add default detectors
     detectors.add(new Tokenizer());
     detectors.add(new Reachability());
@@ -78,8 +67,7 @@ public class Reassembler
   /**
    * Command buffer.
    */
-  public CommandBuffer getCommands()
-  {
+  public CommandBuffer getCommands() {
     return commands;
   }
 
@@ -88,8 +76,7 @@ public class Reassembler
    *
    * @param detector code type detector
    */
-  public void add(IDetector detector)
-  {
+  public void add(IDetector detector) {
     Assert.notNull(detector, "Precondition: detector != null");
 
     detectors.add(detector);
@@ -99,8 +86,7 @@ public class Reassembler
    * All detectors.
    * Just for the mapper.
    */
-  List<IDetector> getDetectors()
-  {
+  List<IDetector> getDetectors() {
     return detectors;
   }
 
@@ -109,8 +95,7 @@ public class Reassembler
    *
    * @param input program with start address
    */
-  public void reassemble(InputStream input) throws IOException
-  {
+  public void reassemble(InputStream input) throws IOException {
     Assert.notNull(input, "Precondition: input != null");
 
     reassemble(FileCopyUtils.copyToByteArray(input));
@@ -121,8 +106,7 @@ public class Reassembler
    *
    * @param program program with start address
    */
-  public void reassemble(byte[] program) throws IOException
-  {
+  public void reassemble(byte[] program) throws IOException {
     Assert.notNull(program, "Precondition: program != null");
     Assert.isTrue(program.length >= 2, "Precondition: program.length >= 2");
 
@@ -138,8 +122,7 @@ public class Reassembler
    * @param startAddress start address of code
    * @param code program without start address
    */
-  public void reassemble(int startAddress, InputStream code) throws IOException
-  {
+  public void reassemble(int startAddress, InputStream code) throws IOException {
     Assert.notNull(code, "Precondition: input != null");
 
     reassemble(startAddress, FileCopyUtils.copyToByteArray(code));
@@ -151,13 +134,11 @@ public class Reassembler
    * @param startAddress start address of code
    * @param code program without start address
    */
-  public void reassemble(int startAddress, byte[] code) throws IOException
-  {
+  public void reassemble(int startAddress, byte[] code) throws IOException {
     Assert.isTrue(startAddress >= 0, "Precondition: startAddress >= 0");
     Assert.notNull(code, "Precondition: code != null");
 
-    if (startAddress == 0x0801)
-    {
+    if (startAddress == 0x0801) {
       // TODO check for basic header
     }
 
@@ -167,8 +148,7 @@ public class Reassembler
   /**
    * Reassemble again.
    */
-  public void reassemble()
-  {
+  public void reassemble() {
     reassemble(commands);
   }
 
@@ -177,15 +157,13 @@ public class Reassembler
    *
    * @param commands command buffer
    */
-  public void reassemble(CommandBuffer commands)
-  {
+  public void reassemble(CommandBuffer commands) {
     Assert.notNull(commands, "Precondition: commands != null");
 
     this.commands = commands;
 
     boolean change = true;
-    for (int count = 0; change && count < 10; count++)
-    {
+    for (int count = 0; change && count < 10; count++) {
       change = detectCodeType();
       System.out.println(count);
     }
@@ -198,24 +176,21 @@ public class Reassembler
    *
    * @return whether a code label has been altered
    */
-  private boolean detectCodeType()
-  {
+  private boolean detectCodeType() {
     boolean change = false;
 
-    for (IDetector detector : detectors)
-    {
+    for (IDetector detector : detectors) {
       boolean detectorHit = detector.detect(commands);
-      if (detectorHit)
-      {
+      if (detectorHit) {
         System.out.println(detector.getClass().getSimpleName());
       }
       change |= detectorHit;
     }
 
-    // TODO iseahe: setType() for label locations?
-    // TODO iseahe: setType(OPCODE/CODE) for OpcodeCommands?
-    // TODO iseahe: detect self modifying code
-    // TODO iseahe: detect JSR with parameters afterwards
+    // TODO mh: setType() for label locations?
+    // TODO mh: setType(OPCODE/CODE) for OpcodeCommands?
+    // TODO mh: detect self modifying code
+    // TODO mh: detect JSR with parameters afterwards
 
     return change;
   }
@@ -223,23 +198,18 @@ public class Reassembler
   /**
    * Combine commands, if possible.
    */
-  private void combine()
-  {
+  private void combine() {
     Assert.notNull(commands, "Precondition: buffer != null");
 
     CommandIterator iter = new CommandIterator(commands);
 
     ICommand lastCommand = null;
-    while (iter.hasNextCommand())
-    {
+    while (iter.hasNextCommand()) {
       ICommand command = iter.nextCommand();
-      if (!iter.hasLabel() && lastCommand != null && lastCommand.combineWith(command))
-      {
+      if (!iter.hasLabel() && lastCommand != null && lastCommand.combineWith(command)) {
         // TODO let command buffer handle this functionality?
         iter.removeCommand();
-      }
-      else
-      {
+      } else {
         lastCommand = command;
       }
     }
@@ -250,8 +220,7 @@ public class Reassembler
    *
    * @param output writer to write output to
    */
-  public void write(Writer output) throws IOException
-  {
+  public void write(Writer output) throws IOException {
     Assert.notNull(commands, "Precondition: buffer != null");
     Assert.notNull(output, "Precondition: output != null");
 
@@ -263,40 +232,31 @@ public class Reassembler
 
     // external labels
     Collection<ExternalLabel> externalReferences = new TreeSet<ExternalLabel>(commands.getExternalLabels());
-    for (ExternalLabel externalReference : externalReferences)
-    {
+    for (ExternalLabel externalReference : externalReferences) {
       output.append(externalReference.toString()).append(" = ").append(hex(externalReference.getAddress())).append("\n");
     }
     output.append("\n");
 
     // code
     StringBuilder line = new StringBuilder(80);
-    while (iter.hasNextCommand())
-    {
+    while (iter.hasNextCommand()) {
       ICommand command = iter.nextCommand();
       int pc = command.getAddress();
 
       line.setLength(0);
 
       // debug output: prefixes
-      if (command == null)
-      {
+      if (command == null) {
         line.append("?");
-      }
-      else
-      {
-        if (!command.isReachable())
-        {
+      } else {
+        if (!command.isReachable()) {
           line.append("U");
         }
-        for (int i = 1; i < command.getSize(); i++)
-        {
-          if (commands.hasCodeLabel(pc + i))
-          {
+        for (int i = 1; i < command.getSize(); i++) {
+          if (commands.hasCodeLabel(pc + i)) {
             line.append("C");
           }
-          if (commands.hasDataLabel(pc + i))
-          {
+          if (commands.hasDataLabel(pc + i)) {
             line.append("D");
           }
         }
@@ -307,19 +267,17 @@ public class Reassembler
       // debug output: byte representation of command
       line.append(hexWordPlain(pc));
       List<Integer> data = command.toBytes();
-      for (int i = 0; i < data.size() && i < 3; i++)
-      {
+      for (int i = 0; i < data.size() && i < 3; i++) {
         line.append(" ");
         line.append(hexBytePlain(data.get(i)));
       }
       fillSpaces(line, 21);
-      line.append(data.size() > 3? "..." : "   ");
+      line.append(data.size() > 3 ? "..." : "   ");
       line.append(" | ");
 
       // reassembler output
       ILabel label = iter.getLabel();
-      if (label != null)
-      {
+      if (label != null) {
         // TODO mh: check length of label?
         line.append(label.toString(pc)).append(":");
       }
@@ -327,12 +285,9 @@ public class Reassembler
       fillSpaces(line, 40);
       output.write(line.toString());
 
-      if (command != null)
-      {
+      if (command != null) {
         output.append(command.toString(commands));
-      }
-      else
-      {
+      } else {
         // TODO mh: log error?
         output.append("???");
       }
@@ -349,10 +304,8 @@ public class Reassembler
    * @param line line
    * @param num limit
    */
-  private void fillSpaces(StringBuilder line, int num)
-  {
-    while (line.length() < num)
-    {
+  private void fillSpaces(StringBuilder line, int num) {
+    while (line.length() < num) {
       line.append(' ');
     }
   }
