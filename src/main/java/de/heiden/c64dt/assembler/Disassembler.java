39f9cd52126e3b647134bb5a5a63ea7fc2d1d5ea
markus <markus@markusheiden.de>
2013-09-18 17:17:13 +0200

    Extracted Dumper

--- src/main/java/de/heiden/c64dt/assembler/Disassembler.java
+++ src/main/java/de/heiden/c64dt/assembler/Disassembler.java
@@ -1,6 +1,5 @@
 package de.heiden.c64dt.assembler;
 
-import de.heiden.c64dt.charset.C64Charset;
 import de.heiden.c64dt.util.NonClosingBufferedWriter;
 import org.springframework.util.Assert;
 
@@ -73,64 +72,5 @@ public class Disassembler {
       output.append("        ???");
     }
     output.newLine();
-  }
-
-  //
-  //
-  //
-
-
-  /**
-   * Dump memory.
-   *
-   * @param code Code buffer
-   * @param output output for reassembled code
-   */
-  public void dump(ICodeBuffer code, Writer output) throws IOException {
-    Assert.notNull(code, "Precondition: code != null");
-    Assert.notNull(output, "Precondition: output != null");
-
-    try (BufferedWriter out = new NonClosingBufferedWriter(output)) {
-      dump(code, out);
-    }
-    output.flush();
-  }
-
-  /**
-   * Dump memory.
-   *
-   * @param code Code buffer
-   * @param output output for reassembled code
-   */
-  private void dump(ICodeBuffer code, BufferedWriter output) throws IOException {
-    Assert.notNull(code, "Precondition: code != null");
-    Assert.notNull(output, "Precondition: output != null");
-
-    C64Charset charset = C64Charset.LOWER;
-
-    StringBuilder chars = new StringBuilder(16);
-    while (code.hasMore()) {
-      output.append(hexWordPlain(code.getCurrentAddress()));
-      output.append("  ");
-      for (int i = 0; i < 16; i++) {
-        if (code.hasMore()) {
-          int b = code.readByte();
-          output.append(hexBytePlain(b));
-          output.append(" ");
-
-          char c = charset.toChar((byte) b);
-          chars.append(c != 0 ? c : '.');
-        } else {
-          output.append("   ");
-        }
-      }
-
-      output.append("  ");
-      output.append(chars);
-
-      output.newLine();
-
-      chars.setLength(0);
-    }
   }
 }
