b522062edc5b819172b1c2b2f92d3af1ed9fe7a9
Markus Heiden <markus@markusheiden.de>
2023-05-11 21:27:32 +0200

    Bump annotations

--- reassembler-gui/src/main/java/de/heiden/c64dt/reassembler/gui/ReassemblerView.java
+++ reassembler-gui/src/main/java/de/heiden/c64dt/reassembler/gui/ReassemblerView.java
@@ -6,7 +6,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import javax.swing.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
