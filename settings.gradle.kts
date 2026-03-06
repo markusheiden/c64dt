61d1f8c1170024b992b2e43396f5d5881963bff1
Markus Heiden <markus@markusheiden.de>
2026-03-06 23:28:19 +0100

    Use explicit submodule declarations

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,12 @@
 rootProject.name = "c64dt"
 
-// Include all subfolders which contain a Gradle build file with the name of the folder
-rootProject.projectDir
-    .walk()
-    .maxDepth(1)
-    .filter { it.isDirectory && it != rootProject.projectDir }
-    .forEach { dir ->
-        val moduleName = dir.name
-        val gradleFileRegex = Regex("${Regex.escape(moduleName)}\\.gradle.kts")
-
-        val buildFile = dir.listFiles()?.find { file ->
-            file.isFile && gradleFileRegex.matches(file.name)
-        }
-
-        if (buildFile != null) {
-            println("[${rootProject.name}] Adding subproject '$moduleName'")
-
-            include(moduleName)
-            project(":$moduleName").projectDir = dir
-            project(":$moduleName").buildFileName = buildFile.name
-        }
-    }
+include("assembler")
+include("browser")
+include("bytes")
+include("charset")
+include("common")
+include("disk")
+include("gui")
+include("net")
+include("reassembler")
+include("reassembler-gui")
