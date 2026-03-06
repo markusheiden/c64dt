aebd559a0a0a9dcd1d5287afba751c33ccf830a9
Markus Heiden <markus@markusheiden.de>
2026-03-06 23:12:17 +0100

    Rework auto detection of submodules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,29 +1,23 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-    }
-}
-
 rootProject.name = "c64dt"
 
-// Include all subfolder which contains a Gradle build file with the name of the folder
-rootProject.projectDir.listFiles()?.filter { it.isDirectory }?.forEach { dir ->
-    val dirName = dir.name
-    if (dir.resolve("$dirName.gradle.kts").exists()) {
-        println("[${rootProject.name}] Adding subproject '$dirName'")
+// Include all subfolders which contain a Gradle build file with the name of the folder
+rootProject.projectDir
+    .walk()
+    .maxDepth(1)
+    .filter { it.isDirectory && it != rootProject.projectDir }
+    .forEach { dir ->
+        val moduleName = dir.name
+        val gradleFileRegex = Regex("${Regex.escape(moduleName)}\\.gradle.kts")
 
-        include(dirName)
-        project(":$dirName").apply {
-            projectDir = dir
-            buildFileName = "$dirName.gradle.kts"
+        val buildFile = dir.listFiles()?.find { file ->
+            file.isFile && gradleFileRegex.matches(file.name)
+        }
+
+        if (buildFile != null) {
+            println("[${rootProject.name}] Adding subproject '$moduleName'")
+
+            include(moduleName)
+            project(":$moduleName").projectDir = dir
+            project(":$moduleName").buildFileName = buildFile.name
         }
     }
-}
